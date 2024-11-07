5fa71550bdaafa1c1a63fe3458a8461cc5b0bb03
Yooshyasha <mejzikrejzi66@gmail.com>
2024-11-08 01:59:35 +0500

    auth

---
+++ src/main/kotlin/com/yooshyasha/authhwid/security/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package com.yooshyasha.authhwid.security
+
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+) {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.csrf { csrf -> csrf.disable()}
+            .authorizeHttpRequests { auth ->
+                auth.requestMatchers("/v1/keys/createKey").authenticated()
+                auth.anyRequest().permitAll()
+            }.csrf { it.disable() }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+        return http.build()
+    }
+}
