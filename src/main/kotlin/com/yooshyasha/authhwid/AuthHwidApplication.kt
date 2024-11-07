bf2d9fb71bed4dc515065bba851df3e3f65661f5
Yooshyasha <mejzikrejzi66@gmail.com>
2024-11-08 00:26:23 +0500

    Initial commit

---
+++ src/main/kotlin/com/yooshyasha/authhwid/AuthHwidApplication.kt
@@ -0,0 +1,11 @@
+package com.yooshyasha.authhwid
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthHwidApplication
+
+fun main(args: Array<String>) {
+    runApplication<AuthHwidApplication>(*args)
+}
---
+++ src/test/kotlin/com/yooshyasha/authhwid/AuthHwidApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yooshyasha.authhwid
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthHwidApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
